b3b59d706e6fa266e50c81b93f008141440b7b9f
James Farrell <info@jamespfarrell.com>
2017-06-30 00:12:01 +0200

    Type ahead working

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="hello">
     <h1>Search for a github user</h1>
-    <!--<h1>{{ msg }}</h1>-->
     <h2>Please search for a github user below</h2>
-    
+    <h4>Custom templates for results</h4>
+    <h4>Asynchronous results</h4>
+    <!--<typeahead
+            placeholder="Address, async via maps.googleapis.com"
+            async-key="results"
+            src="https://maps.googleapis.com/maps/api/geocode/json?address="
+            :template="asyncTemplate"
+            :on-hit="googleCallback"
+    ></typeahead>-->
+    <!--<span v-on:keyup="githubCallback">-->
+    <typeahead
+            placeholder="Github users, async via api.github.com"
+            async-key="items"
+            src="https://api.github.com/search/users?q="
+            :template="githubTemplate"
+            :on-hit="githubCallback"
+            async="https://api.github.com/search/users?q="
+    ></typeahead>
+     <!-- </span>-->
+   <!-- <typeahead :data="USstate" placeholder="USA states"></typeahead>-->
+
   </div>
 </template>
 
 <script>
+import typeahead from 'vue-strap/src/Typeahead';
+import 'bootstrap-css-only/css/bootstrap.css';
+/* import '../assets/css/Typeahead.css'; */
+
 export default {
   name: 'hello',
+  components: {
+    typeahead,
+  },
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      githubTemplate: '<img width="18px" height="18px" :src="item.avatar_url"/><span>{{item.login}}</span>',
     };
+  },
+  methods: {
+    githubCallback() {
+
+
+    },
   },
 };
 </script>
@@ -37,4 +69,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
